d98b64a87460a86731b08c7fd7736ae79a585f0c
HEUGUET-Antonin <antonin.heuguet@edu.univ-fcomte.fr>
2022-06-02 08:04:37 +0200

    Netilfy

--- src/components/CardWinter.vue
+++ src/components/CardWinter.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="z-10">
       <img
-        src="\winnterzuko.png"
+        src="/winnterzuko.png"
         alt="WinterZuko"
         class="object-cover border-2 border-black w-full h-36"
       />
